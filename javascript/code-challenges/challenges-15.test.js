bcf899d91cc916ad7c0a9e742cf1d3b46d520deb
fawzi-shiyyab19 <fawziakef@icloud.com>
2022-08-10 21:17:15 +0300

    lab15fix

--- javascript/code-challenges/challenges-15.test.js
+++ javascript/code-challenges/challenges-15.test.js
@@ -144,7 +144,18 @@ Here is a sample board:
 ------------------------------------------------------------------------------------------------ */
 
 const detectTicTacToeWin = (board) => {
-  // Solution code here...
+  function helpCheck(row1, col1, row2, col2, row3, col3) {
+    return board[row1][col1] === board[row2][col2] && board[row2][col2] === board[row3][col3] && board[row1][col1] !== '';
+  }
+  for (let i = 0; i < board.length; i++) {
+    if (helpCheck(i, 0, i, 1, i, 2)) return true;
+  }
+  for (let i = 0; i < board.length; i++) {
+    if (helpCheck(0, i, 1, i, 2, i)) return true;
+  }
+  if (helpCheck(0, 0, 1, 1, 2, 2)) return true;
+  if (helpCheck(0, 2, 1, 1, 2, 0)) return true;
+  return false;
 };
 
 /* ------------------------------------------------------------------------------------------------
